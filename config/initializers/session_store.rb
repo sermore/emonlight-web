b0a1bd1004dcc2f1169738b44f4ba86e281f6609
Sergio Moretti <sergio@reliqs.net>
2015-08-16 21:09:42 +0200

    setup openshift configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_emonlight-web_session'
+../../.openshift/lib/session_store.rb
